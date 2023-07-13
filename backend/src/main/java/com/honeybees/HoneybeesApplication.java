8f997e91b14f92d4082e4cca69ed96c2d5e15916
minkyuu <minkyu023@gmail.com>
2023-07-13 16:48:39 +0900

    Chore: BE 초기 환경 설정

---
+++ backend/src/main/java/com/honeybees/HoneybeesApplication.java
@@ -0,0 +1,13 @@
+package com.honeybees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneybeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneybeesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/honeybees/HoneybeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honeybees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneybeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
